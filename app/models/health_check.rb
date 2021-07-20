eff2d235ed18aec5330e036b6b50fdd1a850a5d7
momoka.kai <momoppiiiii0718@gmail.com>
2021-07-20 07:12:48 +0000

    commit for heroku

--- app/models/health_check.rb
+++ app/models/health_check.rb
@@ -1,5 +1,5 @@
 class HealthCheck < ApplicationRecord
   belongs_to :user
-  validates :templature, presence: true
+  validates :templature, presence: true, numericality: { in: 30..50 }
   validates :respiratory_symptom, :throat_nasus_eyes_symptom, :digestive_symptom, :other, :memo, length: { maximum: 255 }
 end
