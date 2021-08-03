df1d55f13e45015c175b8cb1e4af211f5e16bb95
momoka.kai <momoppiiiii0718@gmail.com>
2021-08-03 15:25:54 +0000

    improved index

--- app/controllers/health_checks_controller.rb
+++ app/controllers/health_checks_controller.rb
@@ -2,10 +2,18 @@ class HealthChecksController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :edit, :update]
   def index
     @health_checks = current_user.health_checks.all.order(id: :desc).page(params[:page]).per(10)
+    @respiratory_symptoms = ["せき", "息切れ", "胸部の痛みや圧迫感", "特になし"]
+    @throat_nasus_eyes_symptoms = ["のどの痛み", "鼻づまり", "結膜炎", "味やにおいを感じにくい", "特になし"]
+    @digestive_symptoms = ["吐き気や嘔吐", "腹痛", "下痢", "特になし"]
+    @others = ["倦怠感", "筋肉や関節の痛み", "頭痛", "発疹", "めまい", "悪寒", "食欲不振", "睡眠障害", "精神症状", "特になし"]
   end
   
   def new
     @health_check = HealthCheck.new
+    @respiratory_symptoms = ["せき", "息切れ", "胸部の痛みや圧迫感"]
+    @throat_nasus_eyes_symptoms = ["のどの痛み", "鼻づまり", "結膜炎", "味やにおいを感じにくい"]
+    @digestive_symptoms = ["吐き気や嘔吐", "腹痛", "下痢"]
+    @others = ["倦怠感", "筋肉や関節の痛み", "頭痛", "発疹", "めまい", "悪寒", "食欲不振", "睡眠障害", "精神症状"]
   end
   
   def create
@@ -34,12 +42,16 @@ class HealthChecksController < ApplicationController
   
   def update
     @health_check = HealthCheck.find(params[:id])
+    @respiratory_symptoms = ["特になし","せき", "息切れ", "胸部の痛みや圧迫感"]
+    @throat_nasus_eyes_symptoms = ["特になし","のどの痛み", "鼻づまり", "結膜炎", "味やにおいを感じにくい"]
+    @digestive_symptoms = ["特になし","吐き気や嘔吐", "腹痛", "下痢"]
+    @others = ["特になし","倦怠感", "筋肉や関節の痛み", "頭痛", "発疹", "めまい", "悪寒", "食欲不振", "睡眠障害", "精神症状"]
     if @health_check.update(health_check_params)
       flash[:success] = '更新されました'
       redirect_to health_checks_url
     else
       flash.now[:danger] = '更新できませんでした'
-      render :edit
+      render 'edit'
     end
   end
   
--- app/models/health_check.rb
+++ app/models/health_check.rb
@@ -1,5 +1,5 @@
 class HealthCheck < ApplicationRecord
   belongs_to :user
-  validates :templature, presence: true, numericality:  { greater_than_or_equal_to: 30, less_than_or_equal_to: 50 }
+  validates :templature, presence: true, numericality:  { greater_than_or_equal_to: 30, less_than_or_equal_to: 45 }
   validates :respiratory_symptom, :throat_nasus_eyes_symptom, :digestive_symptom, :other, :memo, length: { maximum: 255 }
 end
