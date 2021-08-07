4d16e168ae9ce74d16147ffc1a8f9899b3029560
momoka.kai <momoppiiiii0718@gmail.com>
2021-08-07 09:56:28 +0000

    fixed error

--- app/controllers/health_checks_controller.rb
+++ app/controllers/health_checks_controller.rb
@@ -18,7 +18,10 @@ class HealthChecksController < ApplicationController
   
   def create
     @health_check = current_user.health_checks.build(health_check_params)
-    
+    @respiratory_symptoms = ["せき", "息切れ", "胸部の痛みや圧迫感"]
+    @throat_nasus_eyes_symptoms = ["のどの痛み", "鼻づまり", "結膜炎", "味やにおいを感じにくい"]
+    @digestive_symptoms = ["吐き気や嘔吐", "腹痛", "下痢"]
+    @others = ["倦怠感", "筋肉や関節の痛み", "頭痛", "発疹", "めまい", "悪寒", "食欲不振", "睡眠障害", "精神症状"]
     if @health_check.save
       redirect_to complete_health_check_url(id: @health_check)
     else
