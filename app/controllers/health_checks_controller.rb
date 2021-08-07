734aeed96fdc347fdbbc225553a30973af52e928
momoka.kai <momoppiiiii0718@gmail.com>
2021-08-07 07:01:35 +0000

    fixed br_tag

--- app/controllers/health_checks_controller.rb
+++ app/controllers/health_checks_controller.rb
@@ -2,7 +2,7 @@ class HealthChecksController < ApplicationController
   before_action :require_user_logged_in, only: [:index, :edit, :update]
   def index
     @health_checks = current_user.health_checks.all.order(id: :desc).page(params[:page]).per(10)
-     @respiratory_symptoms = ["せき", "息切れ", "胸部の痛みや圧迫感"]
+    @respiratory_symptoms = ["せき", "息切れ", "胸部の痛みや圧迫感"]
     @throat_nasus_eyes_symptoms = ["のどの痛み", "鼻づまり", "結膜炎", "味やにおいを感じにくい"]
     @digestive_symptoms = ["吐き気や嘔吐", "腹痛", "下痢"]
     @others = ["倦怠感", "筋肉や関節の痛み", "頭痛", "発疹", "めまい", "悪寒", "食欲不振", "睡眠障害", "精神症状"]
