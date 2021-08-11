1ca125096707bbe4a598b88ede7d5ea688f0bad3
momoka.kai <momoppiiiii0718@gmail.com>
2021-08-11 04:46:15 +0000

    precence true

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
                     format: { with: /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i },
                     uniqueness: { case_sensitive: false }
   validates :belong, length: { maximum: 20 }
-  validates :vaccine, length: { maximum: 5 }
+  validates :vaccine, presence: true, length: { maximum: 5 }
   has_secure_password
   
   has_many :health_checks
